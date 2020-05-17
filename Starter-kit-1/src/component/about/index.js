57cf33564e761f6389f55120af5322524226e8e3
ameen-alam <ameenalam202@gmail.com>
2020-05-17 21:43:20 +0500

    Fetch API in React Hooks

--- Starter-kit-1/src/component/about/index.js
+++ Starter-kit-1/src/component/about/index.js
@@ -13,11 +13,14 @@ const About = () => {
     useEffect(() => {
         // post
         const params = {
-            id: "ameenalam123",
-            name: "ameen"
+            id: 4,
+            first_name: "sheikh ameen",
+            last_name: "alam",
+            email: "sheikhameen@gmail.com"
         }
 
-        axios.post('https://jsonplaceholder.typicode.com/todos/', params)
+        //  post
+        axios.post('http://localhost:3000/employees', params)
             .then((res) => {
                 console.log(res.data, "res")
             })
@@ -25,8 +28,27 @@ const About = () => {
                 console.log(error, "error")
             ))
 
+        //  update
+        axios.put(`http://localhost:3000/employees/${params.id}`, params)
+            .then((res) => {
+                console.log(res.data, "res")
+            })
+            .catch((error) => (
+                console.log(error, "error")
+            ))
+
+
+        //  delete
+        // axios.delete(`http://localhost:3000/employees/1`)
+        //     .then((res) => {
+        //         console.log(res.data, "res")
+        //     })
+        //     .catch((error) => (
+        //         console.log(error, "error")
+        //     ))
+
         //  get
-        axios.get('https://jsonplaceholder.typicode.com/todos/')
+        axios.get('http://localhost:3000/employees')
             .then((res) => {
                 setFecthAPI(res.data)
                 console.log(res.data, "res")
@@ -35,8 +57,17 @@ const About = () => {
                 console.log(error, "error")
             ))
 
-
-
+        // get by fetch
+        fetch('http://localhost:3000/employees')
+            .then((res) => {
+                return res.json()
+            })
+            .then((res2) => {
+                console.log(res2, "res2")
+            })
+            .catch((error) => (
+                console.log(error, "error")
+            ))
 
 
 
@@ -51,51 +82,28 @@ const About = () => {
 
 
 
-
-    // const makeGetRequest = async () => {
-
-
-
-
-    // *********** Exmaple 1 Get Method
-    // Get
-
-    // let res = await axios.get('https://jsonplaceholder.typicode.com/todos/')
-    // let data = res
-    // console.log(data, "data")
-
-    // *********** -end- ********** //
+    // makeGetRequest
+    const makeGetRequest = async () => {
 
 
 
-
-    // *********** Exmaple 2 Get head
-    // head
-
-    // let res = await axios.head('https://jsonplaceholder.typicode.com/todos/')
-    // console.log( res.status ,"Status")
-    // console.log( res.headers["content-type"] ,"content-type")
-    // console.log( res.headers["last-modified"] ,"last-modified")
-
-    // *********** -end- ********** //
+        // post // edit
+        const params = {
+            id: 4,
+            first_name: "Sheikh",
+            last_name: "ameen",
+            email: "sheikhameen@gmail.com"
+        }
 
 
+        // *********** Exmaple 1 post Method
+        //  post
+
+        let res1 = await axios.post('http://localhost:3000/employees', params)
+        console.log(res1.data, "res")
 
 
-
-
-    // *********** Exmaple 3 basic API
-    // basic API
-
-    // const config = {
-    //     method: 'get',
-    //     url: 'https://jsonplaceholder.typicode.com/todos/',
-    //     // headers: { 'User-Agent': 'Console app' }
-    // }
-    // let res = await axios(config)
-    // console.log(res, 'res')
-
-    // *********** -end- ********** //
+        // *********** -end- ********** //
 
 
 
@@ -103,38 +111,149 @@ const About = () => {
 
 
 
-    // *********** Exmaple 3 Post Method
-    // post
 
-    // const params = {
-    //     id : "ameenalam123",
-    //     name : "ameen"
-    // }
-    // let res = await axios.post("https://jsonplaceholder.typicode.com/todos/", params)
-    // console.log(res.data)
+        // *********** Exmaple 2 put Method
+        //  edit
 
-    // *********** -end- ********** //
+
+        let res2 = await axios.put(`http://localhost:3000/employees/${params.id}`, params)
+        console.log(res2.data, "res")
+
+        // *********** -end- ********** //
 
 
 
 
 
 
-    // *********** Exmaple 3 Delete Method
-    // delete
-
-    // let res = await axios.delete("https://jsonplaceholder.typicode.com/todos/id")
-    // console.log(res.data)
-
-    // *********** -end- ********** //
 
 
 
 
 
-    // }
+        // *********** Exmaple 3 delete Method
+        //  delete
 
+        let res3 = await axios.delete(`http://localhost:3000/employees/1`)
+        console.log(res3.data, "res")
+
+        // *********** -end- ********** //
+
+
+
+
+
+
+
+
+
+
+
+        // *********** Exmaple 4 get Method
+        //  get
+
+        let res4 = await axios.get('http://localhost:3000/employees')
+        console.log(res4.data, "res")
+
+        // *********** -end- ********** //
+
+
+
+
+
+
+
+
+
+
+
+        // *********** Exmaple 5 Get Method
+        // Get
+
+        let res5 = await axios.get('https://jsonplaceholder.typicode.com/todos/')
+        let data = res5
+        console.log(data, "data")
+
+        // *********** -end- ********** //
+
+
+
+
+
+
+
+
+
+        // *********** Exmaple 6 Get head
+        // head
+
+        let res6 = await axios.head('https://jsonplaceholder.typicode.com/todos/')
+        console.log(res6.status, "Status")
+        console.log(res6.headers["content-type"], "content-type")
+        console.log(res6.headers["last-modified"], "last-modified")
+
+        // *********** -end- ********** //
+
+
+
+
+
+
+
+
+
+        // *********** Exmaple 7 basic API
+        // basic API
+
+        const config = {
+            method: 'get',
+            url: 'https://jsonplaceholder.typicode.com/todos/',
+            // headers: { 'User-Agent': 'Console app' }
+        }
+        let res7 = await axios(config)
+        console.log(res7, 'res')
+
+        // *********** -end- ********** //
+
+
+
+
+
+
+
+
+
+        // *********** Exmaple 8 Post Method
+        // post
+
+
+        let res8 = await axios.post("https://jsonplaceholder.typicode.com/todos/", params)
+        console.log(res8.data, "res8.data")
+
+        // *********** -end- ********** //
+
+
+
+
+
+
+        // *********** Exmaple 9 Delete Method
+        // delete
+
+        // let res9 = await axios.delete("https://jsonplaceholder.typicode.com/todos/id")
+        // console.log(res9.data, "res9.data")
+
+        // *********** -end- ********** //
+
+
+
+
+
+    }
+    // end makeGetRequest
+    console.log(makeGetRequest, "makeGetRequest")
     console.log(fecthAPI, "fecthAPI")
+
     return (
         <div>
             <div id="carouselExampleCaptions" className="carousel slide" >
@@ -163,7 +282,7 @@ const About = () => {
                     {/* react hook useState print fecthAPI array []  */}
                     {
                         fecthAPI.map((val, key) => (
-                            <li key={key} id={val.id} >{val.title}</li>
+                            <li key={key} id={val.id} >{val.first_name} {val.last_name}</li>
                         ))
                     }
 
